fde0663a4a3f7eb277e137a11cea632e7c04aef4
Débora Nortes <deborasouto2@gmail.com>
2024-12-06 23:17:05 -0300

    Adiciona projeto desafio-codelab-vue

---
+++ desafio-codelab-vue/README.md
@@ -0,0 +1,45 @@
+# Projeto Vai na Web - Desafio com Vite, Vue e Sass ![Vue](https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D) ![Vite](https://img.shields.io/badge/Vite-646CFF?style=for-the-badge&logo=vite&logoColor=FFD62E) ![Sass](https://img.shields.io/badge/Sass-CC6699?style=for-the-badge&logo=sass&logoColor=white) ![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+Este projeto é um desafio do **Vai na Web** e **Iuri Code**, desenvolvido a partir de um design no Figma. Utilizamos Vue.js, Vite e Sass para criar uma aplicação reativa e estilizada.
+
+## Funcionalidades do Vue
+
+- **Reatividade:** Utilizamos `ref` para criar variáveis reativas que permitem o comportamento dinâmico na interface.
+- **Componentes:** Estruturamos o código em componentes para melhor organização e reutilização.
+- **Diretivas:** Empregamos diretivas como `v-bind`, `v-if`, `v-for`, e `v-on` para manipular o DOM de maneira declarativa.
+- **Eventos:** Implementamos eventos para responder a interações do usuário, como cliques em ícones.
+
+## Estrutura do Projeto
+
+O projeto foi configurado com **Vite** para aproveitamento de suas características de build rápida e experiência de desenvolvimento otimizada.
+
+### Dependências Utilizadas
+- **Vue.js:** Framework JavaScript progressivo para construção de interfaces de usuário.
+- **Vite:** Ferramenta de build e bundler extremamente rápida.
+- **Sass:** Pré-processador CSS que facilita a escrita de CSS de maneira eficiente e organizada.
+[![Ver no Figma](https://img.shields.io/badge/Ver%20no%20Figma-0ACF83?style=for-the-badge&logo=figma&logoColor=white)](https://www.figma.com/design/C2mrNhGoSuFAtWf0uMzXEp/Desafios---CodeLab-(Copy)?node-id=0-1&node-type=canvas&t=OxJ56zwcwNk2sruT-0)
+
+## Deploy
+![Acesse o Deploy](https://img.shields.io/badge/Acesse%20o%20Deploy-e07b67?style=for-the-badge&logo=appveyor&logoColor=white)
+
+
+## Prévia do Projeto
+![Projeto GIF](./gif.gif)
+
+
+## Lógica Implementada
+
+Implementamos a lógica de alternar entre duas imagens (`icon-heart.png` e `icon-heart-active.png`) ao clicar em um ícone de coração. Além disso, adicionamos uma classe de borda (`border`) para destacar a seção correspondente.
+
+
+
+### Alternar Imagens
+
+Implementamos a lógica de alternar entre duas imagens (`icon-heart.png` e `icon-heart-active.png`) ao clicar em um ícone de coração. Além disso, adicionamos uma classe de borda (`border`) para destacar a seção correspondente.
+
+### Função de Busca
+
+Em construção.
+
+
+
---
+++ desafio-codelab-vue/src/App.vue
@@ -0,0 +1,260 @@
+<script setup>
+import { ref } from "vue";
+import heart from "./assets/images/icon-heart.png";
+import heartActive from "./assets/images/icon-heart-active.png";
+import lupa from "./assets/images/icon-search.svg";
+
+// Variável reativa que armazena o estado de cada coração
+const image = ref([heart, heart, heart]);
+// Inicialmente, todos os corações estão não curtidos
+// Variável reativa que armazena o estado das bordas
+const border = ref([false, false, false]);
+// Função para alternar as imagens do coração ao clicar e ativar/desativar a borda
+const changeImage = (index) => {
+  image.value[index] = image.value[index] === heart ? heartActive : heart;
+  border.value[index] = !border.value[index];
+};
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__content">
+      <h1 class="header__title"><span>Code</span>Lab</h1>
+      <div class="header__search-container">
+        <img :src="lupa" alt="Lupa" />
+        <input
+          type="search"
+          name="search"
+          placeholder="Pesquisar no blog"
+          id="input-search"
+        />
+      </div>
+    </div>
+  </header>
+
+  <main>
+    <section class="news-container" :class="{ border: border[0] }" data-id="1">
+      <div class="news-container__date">
+        <span>17 de ago, 2024</span>
+        <img :src="image[0]" @click="changeImage(0)" alt="Ícone de coração" />
+      </div>
+      <div class="news-container__content">
+        <h2 class="news-container__title">
+          O que é linguagem de programação? Conheça as principais
+        </h2>
+        <p class="news-container__description">
+          Uma das mais populares vertentes da tecnologia da informação, a área
+          de programação segue tendo muita demanda de trabalho justamente pela
+          velocidade com que dispositivos tecnológicos vêm avançando.
+        </p>
+      </div>
+    </section>
+    <section class="news-container" :class="{ border: border[1] }" data-id="2">
+      <div class="news-container__date">
+        <span>12 de jul, 2024</span>
+        <img :src="image[1]" @click="changeImage(1)" alt="Ícone de coração" />
+      </div>
+      <div class="news-container__content">
+        <h2 class="news-container__title">
+          GitHub agora permite fazer login sem precisar de senha
+        </h2>
+        <p class="news-container__description">
+          O GitHub anunciou nesta quarta-feira (12) o acesso a partir das
+          passkeys, método de autenticação sem senhas. A novidade está
+          disponível em uma versão beta pública e pode substituir a autenticação
+          em dois fatores.
+        </p>
+      </div>
+    </section>
+    <section class="news-container" :class="{ border: border[2] }" data-id="3">
+      <div class="news-container__date">
+        <span>21 de jun, 2024</span>
+        <img :src="image[2]" @click="changeImage(2)" alt="Ícone de coração" />
+      </div>
+      <div class="news-container__content">
+        <h2 class="news-container__title">
+          Por que os hiperlinks são azuis em sua maioria?
+        </h2>
+        <p class="news-container__description">
+          Quem navega na internet, certamente já percebeu que ela conta com
+          diversos recursos para tornar a nossa vida mais fácil. Entre essas
+          opções podemos mencionar os hiperlinks – uma palavra ou termo clicável
+          que direciona o leitor.
+        </p>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scoped lang="scss">
+.header {
+  background-color: #17171a;
+}
+
+.header__content {
+  text-align: center;
+  padding: 4rem 3rem;
+  max-width: 130rem;
+  margin: 0 auto;
+}
+
+@media (min-width: 768px) {
+  .header__content {
+    padding: 6rem 4rem;
+  }
+}
+.header__title {
+  font-size: 2.4rem;
+  color: #ffffff;
+}
+@media (min-width: 768px) {
+  .header__title {
+    font-size: 4rem;
+  }
+}
+.header__title span {
+  color: #e07b67;
+}
+.header__search-container {
+  margin-top: 4rem;
+  background-color: #202024;
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 1rem;
+  border: 2px solid #252529;
+}
+@media (min-width: 768px) {
+  .header__search-container {
+    gap: 2rem;
+  }
+}
+.header__search-container img {
+  width: 2rem;
+}
+@media (min-width: 768px) {
+  .header__search-container img {
+    width: 3rem;
+  }
+}
+.header__search-container input {
+  background-color: transparent;
+  width: 100%;
+  max-width: 300px;
+  border: none;
+  font-size: 1.5rem;
+  color: #ffffff;
+}
+@media (min-width: 768px) {
+  .header__search-container input {
+    font-size: 2rem;
+  }
+}
+.header__search-container input::placeholder {
+  color: #afabb6;
+}
+.header__search-container input:focus {
+  outline: none;
+}
+
+main {
+  padding: 0 5rem 3rem 5rem;
+  max-width: 132rem;
+  margin: 5rem auto 0;
+}
+
+@media (max-width: 767px) {
+  main {
+    padding: 0 1rem;
+    margin: 4rem auto 0;
+  }
+  .news-container {
+    margin: 0 auto 3rem;
+    padding: 2rem;
+    max-width: 90%;
+  }
+}
+
+.news-container {
+  background-color: #17171a;
+  margin-bottom: 3rem;
+  border-radius: 1rem;
+  border: 2px solid #252529;
+  padding: 2rem;
+  transition: all 0.3s;
+}
+@media (min-width: 768px) {
+  main .news-container {
+    padding: 3rem;
+    margin-bottom: 4.5rem;
+  }
+}
+.news-container__date {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.news-container__date span {
+  color: #e07b67;
+  font-size: 1.3rem;
+}
+@media (min-width: 768px) {
+  .news-container__date span {
+    font-size: 1.4rem;
+  }
+}
+@media (min-width: 992px) {
+  .news-container__date span {
+    font-size: 1.6rem;
+  }
+}
+.news-container__date .img {
+  font-size: 2.5rem;
+  color: #e07b67;
+}
+@media (min-width: 768px) {
+  .news-container__date .img {
+    font-size: 3rem;
+  }
+}
+.news-container__content {
+  margin-top: 2rem;
+}
+.news-container__title {
+  font-size: 2rem;
+  color: #ffffff;
+  font-weight: 400;
+}
+@media (min-width: 992px) {
+  .news-container__title {
+    font-size: 2.4rem;
+  }
+}
+.news-container__description {
+  margin-top: 1.5rem;
+  font-size: 1.4rem;
+  color: #afabb6;
+  line-height: 2.4rem;
+}
+@media (min-width: 768px) {
+  .news-container__description {
+    font-size: 1.6rem;
+  }
+}
+@media (min-width: 992px) {
+  .news-container__description {
+    font-size: 2rem;
+    line-height: 3.2rem;
+  }
+}
+
+.img {
+  color: #e07b67;
+  cursor: pointer;
+}
+
+.border {
+  border: 2px solid #e07b67;
+}
+</style>
